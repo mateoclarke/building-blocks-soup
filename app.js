981990cebd5403532f6f371cd8e29e1b9559ee97
mateoclarke <code@mateoclarke.com>
2015-01-04 21:04:22 -0600

    Add initial tests

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (request, response){
-	response.send('OK');
+app.get('/', function(request, response) {
+  response.send('OK');
 });
 
-
-app.listen(3000);
+module.exports = app;
